f52247ead512f690e571ca3bef652a041fae8eca
mshachnai <m.shachnai@rutgers.edu>
2023-04-27 00:18:59 -0400

    made argparse options cleaner

--- bpf_verification/src/bpf_alu_jmp_synthesis.py
+++ bpf_verification/src/bpf_alu_jmp_synthesis.py
@@ -23,14 +23,15 @@ def main():
     #and synthesis procedures.
     parsed_toml = toml.load("verification_synth_setup_config.toml")
     #setup argparse and customized options
-    parser = argp.ArgumentParser()
-    parser.add_argument('--kernver', type=str, help="kernel version")
+    bpf_instructions_set = ["BPF_AND", "BPF_OR", "BPF_LSH", "BPF_RSH", "BPF_JLT", "BPF_JLE", "BPF_JEQ", "BPF_JNE", "BPF_JGE", "BPF_JGT", "BPF_JSGE", "BPF_JSGT", "BPF_JSLT", "BPF_JSLE", "BPF_ADD", "BPF_SUB", "BPF_XOR", "BPF_ARSH", "BPF_OR_32", "BPF_AND_32", "BPF_LSH_32", "BPF_RSH_32", "BPF_ADD_32", "BPF_SUB_32", "BPF_XOR_32","BPF_ARSH_32", "BPF_JLT_32",  "BPF_JLE_32", "BPF_JSLT_32", "BPF_JSLE_32", "BPF_JEQ_32", "BPF_JNE_32", "BPF_JGE_32", "BPF_JGT_32", "BPF_JSGE_32", "BPF_JSGT_32"]
+    parser = argp.ArgumentParser(prog="eBPF Verification and Synthesis", epilog="Possible bpf instructions: {BPF_AND,BPF_OR,BPF_LSH,BPF_RSH,BPF_JLT,BPF_JLE,BPF_JEQ,BPF_JNE,BPF_JGE,BPF_JGT,BPF_JSGE,BPF_JSGT,BPF_JSLT,BPF_JSLE,BPF_ADD,BPF_SUB,BPF_XOR,BPF_ARSH,BPF_OR_32,BPF_AND_32,BPF_LSH_32,BPF_RSH_32,BPF_ADD_32,BPF_SUB_32,BPF_XOR_32,BPF_ARSH_32,BPF_JLT_32,BPF_JLE_32,BPF_JSLT_32,BPF_JSLE_32,BPF_JEQ_32,BPF_JNE_32,BPF_JGE_32,BPF_JGT_32,BPF_JSGE_32,BPF_JSGT_32}")
+    parser.add_argument('--kernver', type=str, required=True, help="kernel version")
     parser.add_argument('--json_offset', type=int, help="offset for mapping bpf_reg_states, set to 4 for 4.14 and 5 for everything else")
-    parser.add_argument('--encodings_path', type=str, help="set path to bpf encodings")
+    parser.add_argument('--encodings_path', type=str, required=True, help="set path to bpf encodings produced by llvm-to-smt")
     parser.add_argument('--res_path', type=str, help="set path where to write results")
-    parser.add_argument('--synth_iter', type=int, help="set length k of sequence to synthesize")
-    parser.add_argument('--synth_set', type=str, help="choose instructions use as priors for synthesis (meaning they won't be the last instructions in the sequence)", nargs="*", choices=["BPF_AND", "BPF_OR", "BPF_LSH", "BPF_RSH", "BPF_JLT", "BPF_JLE", "BPF_JEQ", "BPF_JNE", "BPF_JGE", "BPF_JGT", "BPF_JSGE", "BPF_JSGT", "BPF_JSLT", "BPF_JSLE", "BPF_ADD", "BPF_SUB", "BPF_XOR", "BPF_ARSH", "BPF_OR_32", "BPF_AND_32", "BPF_LSH_32", "BPF_RSH_32", "BPF_ADD_32", "BPF_SUB_32", "BPF_XOR_32","BPF_ARSH_32", "BPF_JLT_32",  "BPF_JLE_32", "BPF_JSLT_32", "BPF_JSLE_32", "BPF_JEQ_32", "BPF_JNE_32", "BPF_JGE_32", "BPF_JGT_32", "BPF_JSGE_32", "BPF_JSGT_32"])
-    parser.add_argument('--ver_set', type=str, help="choose instructions to verify", nargs="*", choices=["BPF_AND", "BPF_OR", "BPF_LSH", "BPF_RSH", "BPF_JLT", "BPF_JLE", "BPF_JEQ", "BPF_JNE", "BPF_JGE", "BPF_JGT", "BPF_JSGE", "BPF_JSGT", "BPF_JSLT", "BPF_JSLE", "BPF_ADD", "BPF_SUB", "BPF_XOR", "BPF_ARSH", "BPF_OR_32", "BPF_AND_32", "BPF_LSH_32", "BPF_RSH_32", "BPF_ADD_32", "BPF_SUB_32", "BPF_XOR_32","BPF_ARSH_32", "BPF_JLT_32",  "BPF_JLE_32", "BPF_JSLT_32", "BPF_JSLE_32", "BPF_JEQ_32", "BPF_JNE_32", "BPF_JGE_32", "BPF_JGT_32", "BPF_JSGE_32", "BPF_JSGT_32"])
+    parser.add_argument('--synth_iter', type=int, help="set sequence length to synthesize (1-3)")
+    parser.add_argument('--synth_set', type=str, metavar='bpf instruction', help="choose instructions use as priors for synthesis (meaning they won't be the last instructions in the sequence)", nargs="*", choices=bpf_instructions_set)
+    parser.add_argument('--ver_set', type=str, metavar='bpf instruction', help="choose instructions to verify", nargs="*", choices=bpf_instructions_set)
     args = parser.parse_args()
     #print(args)
 
