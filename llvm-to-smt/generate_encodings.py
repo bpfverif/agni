f8608d31285887a9d8c038377fdaddd197b75eb3
Paul Chaignon <paul.chaignon@gmail.com>
2023-10-16 12:06:03 +0200

    llvm-to-smt: Fix required parameters
    We can't assume that the Linux repository will always be cloned at the
    root of /home so this parameter must be required. We can legitimately
    assume that the user won't move the llvm-passes directory, so that
    parameter can be optional as long as its value is a relative path.
    
    Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

--- llvm-to-smt/generate_encodings.py
+++ llvm-to-smt/generate_encodings.py
@@ -374,11 +374,11 @@ if __name__ == "__main__":
     parser.add_argument("--kernver", help="kernel version", type=str,
                         required=True)
     parser.add_argument("--kernbasedir", help="kernel base directory", type=str,
-                        required=False, default="/home/linux-stable")
+                        required=True)
     parser.add_argument("--outdir", help="output directory", type=str,
                         required=True)
     parser.add_argument("--scriptsdir", help="scripts directory from llvm-to-smt",
-                        type=str, required=False, default="/home/cav23-artifact/llvm-to-smt/llvm-passes")
+                        type=str, required=False, default="llvm-passes")
     parser.add_argument("--specific-op", dest='specific_op',
                         help='single specific BPF op to encode',
                         choices= bpf_alu_ops + bpf_jmp_ops,
