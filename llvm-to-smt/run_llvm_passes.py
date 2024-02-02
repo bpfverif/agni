d52bff54f59417fe1a55f86431834fbc25460d51
Harishankar Vishwanathan <harishankar.vishwanathan@rutgers.edu>
2024-02-02 19:50:29 +0530

    Update LLVMPassRunner's O0 and O1 passes
    LLVMPassRunner runs "O1" passes on the code to perform dce and
    simplify the cfg. Changing this to "Oz" which optimizes further
    for code size.
    
    Additionally, to recall: after performing some lowering passes (like
    PromoteMemcpy and LowerFunnelShifts), we cannot run "Oz" or
    "O1", as this will end up optimizing the lowered code back to
    higher level constructs. We only run some specific opt passes.
    This commit slightly modifies the list of specific opt passes.

--- llvm-to-smt/run_llvm_passes.py
+++ llvm-to-smt/run_llvm_passes.py
@@ -33,22 +33,24 @@ class LLVMPassRunner:
         self.logfile.write(
             "Creating directory and file for op: {}\n".format(self.op))
         try:
+            # TODO, remove try-catch, fail on error
             self.op_dir_fullpath.mkdir()
         except FileExistsError:
             pass
         op_llfile_fullpath = self.op_dir_fullpath.joinpath(
             '{}.ll'.format(self.op))
-        # print(op_llfile_fullpath)
+        print(op_llfile_fullpath)
         copy2(str(self.input_llfile_fullpath), str(op_llfile_fullpath))
         self.curr_llfile_fullpath = op_llfile_fullpath
 
     def run_opt_pass(self, O1=True):
         llvm_opt_fullpath = self.llvmdir_fullpath.joinpath("bin", "opt")
-        cmd_opt_O1 = '''{llvm_opt_fullpath} -O1 --strip-debug --instnamer --stats -S  {input_llfile_fullpath} -o {output_llfile_fullpath}'''
-        cmd_opt_O0 = '''{llvm_opt_fullpath} -S --instnamer --simplifycfg --sroa --mergereturn --dce --deadargelim --memoryssa --always-inline --function-attrs --argpromotion --instcombine {input_llfile_fullpath} -o {output_llfile_fullpath}'''
+        cmd_opt_O1 = '''{llvm_opt_fullpath} -Oz --strip-debug --instnamer --stats -S  {input_llfile_fullpath} -o {output_llfile_fullpath}'''
+        # cmd_opt_O0 = '''{llvm_opt_fullpath} -S --instnamer --simplifycfg --sroa --mergereturn --dce --deadargelim --memoryssa --always-inline --function-attrs --argpromotion --instcombine {input_llfile_fullpath} -o {output_llfile_fullpath}'''
+        cmd_opt_O0 = '''{llvm_opt_fullpath} -S --instnamer --sroa --adce --bdce --dce --globaldce --deadargelim --unreachableblockelim --lowerswitch --function-attrs --argpromotion {input_llfile_fullpath} -o {output_llfile_fullpath}'''
         if O1 == True:
             output_llfile_fullpath = self.curr_llfile_fullpath.parent.joinpath(
-                self.curr_llfile_fullpath.stem + ".O1" + ".ll")
+                self.curr_llfile_fullpath.stem + ".Oz" + ".ll")
             cmd_opt = cmd_opt_O1.format(
                 llvm_opt_fullpath=llvm_opt_fullpath,
                 input_llfile_fullpath=str(self.curr_llfile_fullpath),
@@ -68,7 +70,7 @@ class LLVMPassRunner:
             self.logfile.write("Running opt -O0\n")
             self.logfile.write(cmd_opt)
             self.logfile.write("\n")
-        # print(cmd_opt)
+        print(cmd_opt)
         subprocess.run(cmd_opt, shell=True, check=True, text=True,
                        stdout=self.logfile, stderr=self.logfile_err, bufsize=1)
         self.logfile.write("\nFinished running opt\n")
@@ -89,7 +91,7 @@ class LLVMPassRunner:
         self.logfile.write("Running force_function_early_exit_pass\n")
         self.logfile.write(cmd_force_functions_early_exit_op)
         self.logfile.write("\n")
-        # print(cmd_force_functions_early_exit_op)
+        print(cmd_force_functions_early_exit_op)
         subprocess.run(cmd_force_functions_early_exit_op, shell=True, check=True,
                        text=True, stdout=self.logfile, stderr=self.logfile_err, bufsize=1)
         self.logfile.write("\nFinished force_function_early_exit_pass\n")
@@ -111,7 +113,7 @@ class LLVMPassRunner:
         self.logfile.write("Running remove_functions_calls_pass\n")
         self.logfile.write(cmd_remove_func_calls_op)
         self.logfile.write("\n")
-        # print(cmd_remove_func_calls_op)
+        print(cmd_remove_func_calls_op)
         subprocess.run(cmd_remove_func_calls_op, shell=True, check=True, text=True,
                        stdout=self.logfile, stderr=self.logfile_err, bufsize=1)
         self.logfile.write("\nFinished remove_functions_calls_pass\n")
@@ -133,7 +135,7 @@ class LLVMPassRunner:
         self.logfile.write("Running lower_funnel_shifts_pass\n")
         self.logfile.write(cmd_lower_funnel_shifts_op)
         self.logfile.write("\n")
-        # print(cmd_lower_funnel_shifts_op)
+        print(cmd_lower_funnel_shifts_op)
         subprocess.run(cmd_lower_funnel_shifts_op, shell=True, check=True, text=True,
                        stdout=self.logfile, stderr=self.logfile_err, bufsize=1)
         self.logfile.write("\nFinished lower_funnel_shifts_pass\n")
@@ -155,7 +157,7 @@ class LLVMPassRunner:
         self.logfile.write("Running inline_verifier_func_pass\n")
         self.logfile.write(cmd_inline_verifier_func_op)
         self.logfile.write("\n")
-        # print(cmd_inline_verifier_func_op)
+        print(cmd_inline_verifier_func_op)
         subprocess.run(cmd_inline_verifier_func_op, shell=True, check=True, text=True,
                        stdout=self.logfile, stderr=self.logfile_err, bufsize=1)
         self.logfile.write("\nFinished inline_verifier_func_pass\n")
@@ -177,7 +179,7 @@ class LLVMPassRunner:
         self.logfile.write("Running promote_memcpy_pass\n")
         self.logfile.write(cmd_promote_memcpy_op)
         self.logfile.write("\n")
-        # print(cmd_promote_memcpy_op)
+        print(cmd_promote_memcpy_op)
         subprocess.run(cmd_promote_memcpy_op, shell=True, check=True, text=True,
                        stdout=self.logfile, stderr=self.logfile_err, bufsize=1)
         self.logfile.write("\nFinished promote_memcpy_pass\n")
@@ -198,7 +200,7 @@ class LLVMPassRunner:
         self.logfile.write("Running llvm_to_smt_pass\n")
         self.logfile.write(cmd_llvm_to_smt_op)
         self.logfile.write("\n")
-        # print(cmd_llvm_to_smt_op)
+        print(cmd_llvm_to_smt_op)
         subprocess.run(cmd_llvm_to_smt_op, shell=True, check=True, text=True,
                        stdout=self.logfile, stderr=self.logfile_err, bufsize=1)
         self.logfile.write("\nFinished llvm_to_smt_pass\n")
@@ -214,7 +216,7 @@ class LLVMPassRunner:
         self.logfile.write("Running llvm-extract\n")
         self.logfile.write(" ".join(cmd_extract))
         self.logfile.write("\n")
-        # print(cmd_extract)
+        print(cmd_extract)
         cmdout_extract = subprocess.run(
             cmd_extract, stdout=self.logfile, stderr=self.logfile_err, text=True, bufsize=1, check=True)
         self.logfile.write("\nFinished running llvm-extract\n")
@@ -229,7 +231,7 @@ class LLVMPassRunner:
 
         self.run_remove_functions_calls_pass()
         self.run_opt_pass(O1=True)
-
+        
         self.run_inline_verifier_func_pass()
         self.run_opt_pass(O1=True)
 
@@ -238,11 +240,12 @@ class LLVMPassRunner:
 
         self.run_lower_funnel_shifts_pass()
         self.run_inline_verifier_func_pass()
+        
+        self.run_opt_pass(O1=False)
 
         self.run_llvm_extract()
-
         self.run_llvm_to_smt_pass()
-
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
