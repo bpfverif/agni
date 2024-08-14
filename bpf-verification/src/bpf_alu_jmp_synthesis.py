bpfverif/agni

commit 2e94506a0982b12ecd454b7d9d7e24bd7d0dbd6b

initial flag addition for weak specification

bpf-verification/src/bpf_alu_jmp_synthesis.py
@@ -25,6 +25,7 @@ def main():
     epilog = "Possible bpf instructions: %s" % ", ".join(bpf_instructions_set) + " ----- BPF_SYNC can be used only in ver_set"
     parser = argp.ArgumentParser(prog="eBPF Verification and Synthesis", epilog=epilog)
     parser.add_argument('--kernver', type=str, required=True, help="kernel version")
+    parser.add_argument('--weak_spec', type=int, default=0, choices=[0,1], required=False, help="flag for weakened specification (speculative) to force agni to check both branch paths")
     parser.add_argument('--json_offset', type=int, help="offset for mapping bpf_reg_states, should be set to 4 for v4.14, 5 for v4.15–v6.2, and 3 for v6.3+")
     parser.add_argument('--encodings_path', type=str, required=True, help="set path to bpf encodings produced by llvm-to-smt")
     parser.add_argument('--res_path', type=str, required=True, help="set path to directory where results will be written")
@@ -46,6 +47,7 @@ def main():
         else:
             args.json_offset = 5
     parsed_config["json_offset"] = args.json_offset
+    parsed_config["weak_spec"] = args.weak_spec
     parsed_config["bpf_encodings_path"] = args.encodings_path
     if not os.path.isdir(args.res_path):
         print("'%s' does not exist or is not a directory" % args.res_path, file=sys.stderr)

bpf-verification/lib_reg_bounds_tracking/lib_reg_bounds_tracking.py
@@ -512,6 +512,7 @@ class config_setup:
         self.synth_len2= 0
         self.synth_len3= 0
         self.json_offset = config_file["json_offset"]
+        self.weak_spec = config_file["weak_spec"]
         self.kernel_ver = config_file["kernel_ver"]
         self.encodings_path = config_file["bpf_encodings_path"] + "/" 
         self.write_path = config_file["write_dir_path"] + "/" + config_file["kernel_ver"] + "_res/"

commit 59faf69fe73a7e11b9333276f3c040f2a4b515d6

bpf-verification: Make --weak_spec flag boolean
It looks like the intention was to have this flag as a boolean, so let's
do that to avoid the extra parameter (0, 1).

Fixes: 2e94506a ("initial flag addition for weak specification")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

bpf-verification/src/bpf_alu_jmp_synthesis.py
@@ -25,7 +25,7 @@ def main():
     epilog = "Possible bpf instructions: %s" % ", ".join(bpf_instructions_set) + " ----- BPF_SYNC can be used only in ver_set"
     parser = argp.ArgumentParser(prog="eBPF Verification and Synthesis", epilog=epilog)
     parser.add_argument('--kernver', type=str, required=True, help="kernel version")
-    parser.add_argument('--weak_spec', type=int, default=0, choices=[0,1], required=False, help="flag for weakened specification (speculative) to force agni to check both branch paths")
+    parser.add_argument('--weak-spec', action=argp.BooleanOptionalAction, help="flag for weakened specification (speculative) to force agni to check both branch paths")
     parser.add_argument('--json_offset', type=int, help="offset for mapping bpf_reg_states, should be set to 4 for v4.14, 5 for v4.15–v6.2, and 3 for v6.3+")
     parser.add_argument('--encodings_path', type=str, required=True, help="set path to bpf encodings produced by llvm-to-smt")
     parser.add_argument('--res_path', type=str, required=True, help="set path to directory where results will be written")