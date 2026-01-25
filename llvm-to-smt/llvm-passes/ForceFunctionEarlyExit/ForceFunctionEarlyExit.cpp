5f840b58079e99d4723bbdd58c27e0b634727a6a
Harishankar Vishwanathan <harishankar.vishwanathan@gmail.com>
2026-01-24 22:10:10 -0500

    llvm-to-smt: Deprecate RemoveFunctionCalls pass in favor of ForceFunctionEarlyExit pass

--- llvm-to-smt/llvm-passes/ForceFunctionEarlyExit/ForceFunctionEarlyExit.cpp
+++ llvm-to-smt/llvm-passes/ForceFunctionEarlyExit/ForceFunctionEarlyExit.cpp
@@ -3,97 +3,59 @@
 #define DEBUG_TYPE "force-function-early-exit"
 using namespace llvm;
 
-#define VERIFIER_FUNC_SANITIZE_NEEDED "sanitize_needed"
-#define VERIFIER_FUNC_SANITIZE_VAL_ALU "sanitize_val_alu"
-#define VERIFIER_FUNC_1 "can_skip_alu_sanitation"
-#define VERIFIER_FUNC_2 "update_alu_sanitation_state"
-
 void ForceFunctionEarlyExit::initFunctionEarlyExitMap(char *filePath,
                                                       Module &M) {
   std::ifstream ifs(filePath);
   Json::Reader reader;
   Json::Value obj;
   reader.parse(ifs, obj);
+
   const Json::Value &map = obj["function_returnvalue_map"];
   std::cout << map.size() << std::endl;
+
   for (int i = 0; i < map.size(); i++) {
     std::string funcName = map[i]["function"].asString();
-    int retValue = map[i]["return_value"].asInt();
+    Json::Value retValJson = map[i]["return_value"];
+    // int retValue = map[i]["return_value"].asInt();
     llvm::Function *func = M.getFunction(funcName);
+
     if (func) {
-      ForceFunctionEarlyExit::functionEarlyExitMap.insert({func, retValue});
-      outs() << "func: " << func->getName() << ", retValue: " << retValue
-             << "\n";
+      if (retValJson.isNull()) {
+        ForceFunctionEarlyExit::functionEarlyExitMap.insert(
+            {func, std::nullopt});
+      } else if (retValJson.isBool()) {
+        ForceFunctionEarlyExit::functionEarlyExitMap.insert(
+            {func, retValJson.asBool() ? 1 : 0});
+      } else {
+        ForceFunctionEarlyExit::functionEarlyExitMap.insert(
+            {func, retValJson.asInt()});
+      }
+    } else {
+      outs() << "[initFunctionEarlyExitMap]"
+             << "Function not found: " << funcName << "\n";
     }
   }
-
-#if 0
-  std::ifstream inFileStream;
-  inFileStream.open(filePath);
-  if (!inFileStream) {
-    throw std::runtime_error("File not found: " + std::string(filePath) + "\n");
-  }
-  std::string line;
-  while (std::getline(inFileStream, line)) {
-    auto delimEnd = line.find(":");
-    std::string funcName = line.substr(0, delimEnd);
-    auto retValue = std::stoi(line.substr(delimEnd + 1));
-    llvm::Function *func = M.getFunction(funcName);
-    ForceFunctionEarlyExit::functionEarlyExitMap.insert({func, retValue});
-    outs() << "func: " << func->getName() << "\n";
-    outs() << "retValue: " << retValue << "\n";
-  }
-#endif
 }
 
 bool ForceFunctionEarlyExit::replaceRetInst(llvm::Function &F,
                                             llvm::LLVMContext &CTX,
-                                            int retval) {
-  outs() << "Changing return statement in function: " << F << "\n";
-
-  bool Changed = false;
-  llvm::ReturnInst *retInst;
-  for (llvm::BasicBlock &B : F) {
-    for (llvm::Instruction &I : B) {
-      if (isa<ReturnInst>(&I)) {
-        retInst = dyn_cast<llvm::ReturnInst>(&I);
-      }
-    }
-  }
-  outs() << "retInst (old): " << *retInst << "\n";
-
+                                            std::optional<int> retval) {
   Type *retType = F.getReturnType();
-  IntegerType *retTypeInt = dyn_cast<llvm::IntegerType>(retType);
-  if (!retTypeInt) {
-    throw std::runtime_error("Return type is not an integer\n");
-  }
-
-  auto retTypeIntBitWidth = retTypeInt->getBitWidth();
-  outs() << "retTypeIntBitWidth: " << retTypeIntBitWidth << "\n";
-
-  Value *i32zero = ConstantInt::get(CTX, APInt(retTypeIntBitWidth, retval));
-  outs() << "i32zero" << *i32zero << "\n";
 
   BasicBlock *newEntryBB =
-      BasicBlock::Create(CTX, "newentry", &F, &F.getEntryBlock());
-  llvm::ReturnInst *newRetInst =
-      llvm::ReturnInst::Create(CTX, i32zero, newEntryBB);
-  outs() << "newEntryBB" << *newEntryBB << "\n";
+      BasicBlock::Create(CTX, "early_exit", &F, &F.getEntryBlock());
+  ReturnInst *newRetInst = nullptr;
 
-#if 0
-  retInst->setOperand(0, i32zero);
-  outs() << "retInst: (new)" << *retInst << "\n";
-  outs() << "Changed function: \n" << F << "\n";
-#endif
-
-#if 0
-  llvm::BasicBlock &entryBB = F.getEntryBlock();
-  llvm::ReturnInst *newRetInst =
-      llvm::ReturnInst::Create(CTX, i32zero, &entryBB);
-
-  outs() << "retInst: (new first)" << *newRetInst << "\n";
-  outs() << "Changed function: \n" << F << "\n";
-#endif
+  if (retType->isVoidTy()) {
+    assert(!retval.has_value()); // if return value is void, retval from json
+                                 // should be "null"
+    newRetInst = ReturnInst::Create(CTX, nullptr, newEntryBB);
+  } else if (retType->isIntegerTy()) {
+    Value *retValObj = ConstantInt::get(retType, retval.value());
+    newRetInst = ReturnInst::Create(CTX, retValObj, newEntryBB);
+  } else {
+    throw std::runtime_error("Unsupported return type \n");
+  }
 
   return true;
 }
@@ -109,7 +71,7 @@ ForceFunctionEarlyExit::run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM) {
   if (!functionsEarlyRetValueMapStr) {
     throw std::invalid_argument(
         "No file specified: needs a list of functions and their corresponding "
-        "expected early return values. Please set the "
+        "expected early return values in config.json. Please set the "
         "FUNCTIONS_EARLY_RETVALUE_MAP_TXT environment variable.\n");
   }
 
@@ -118,21 +80,12 @@ ForceFunctionEarlyExit::run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM) {
   for (auto &pair : ForceFunctionEarlyExit::functionEarlyExitMap) {
     outs() << "--------------------\n";
     llvm::Function *f = pair.first;
-    int r = pair.second;
+    std::optional<int> r = pair.second;
+    outs() << "function: " << f->getName() << "\n";
+    outs() << "return type: " << r << "\n";
     Changed |= replaceRetInst(*f, CTX, r);
     outs() << "--------------------\n";
   }
-
-#if 0
-  llvm::Function *functionSanitizeNeeded =
-      M.getFunction(llvm::StringRef(VERIFIER_FUNC_SANITIZE_NEEDED));
-  if (!functionSanitizeNeeded) {
-    outs() << "Function not found: " << VERIFIER_FUNC_SANITIZE_NEEDED << "\n";
-    outs() << "Continuing...\n";
-  } else {
-    Changed |= replaceRetInst(*functionSanitizeNeeded, CTX, 0);
-  }
-#endif
 
   return (Changed ? llvm::PreservedAnalyses::none()
                   : llvm::PreservedAnalyses::all());
--- llvm-to-smt/llvm-passes/ForceFunctionEarlyExit/ForceFunctionEarlyExit.hpp
+++ llvm-to-smt/llvm-passes/ForceFunctionEarlyExit/ForceFunctionEarlyExit.hpp
@@ -1,29 +1,28 @@
 #ifndef LLVM2SMT_FORCE_FUNCTIONS_EARLY_EXIT_H
 #define LLVM2SMT_FORCE_FUNCTIONS_EARLY_EXIT_H
 
-#include <llvm/ADT/PostOrderIterator.h>
-#include <llvm/Analysis/CallGraph.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/Passes/PassBuilder.h>
-#include <llvm/Passes/PassPlugin.h>
 #include <llvm/ADT/ArrayRef.h>
+#include <llvm/ADT/PostOrderIterator.h>
 #include <llvm/ADT/StringRef.h>
+#include <llvm/Analysis/CallGraph.h>
 #include <llvm/IR/Attributes.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Type.h>
 #include <llvm/IR/Value.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/PassPlugin.h>
 
-#include <stdexcept>
-#include <utility>
-#include <unordered_map>
-#include <unordered_set>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
 
 #include <jsoncpp/json/json.h>
-
 
 struct ForceFunctionEarlyExit
     : public llvm::PassInfoMixin<ForceFunctionEarlyExit> {
@@ -34,10 +33,11 @@ struct ForceFunctionEarlyExit
   std::vector<llvm::Function *> functionsToRemoveCallsInstsFrom;
   void initFunctionsToRemove();
   void initfunctionsToRemoveCallsInstsFrom(llvm::Module &M);
-  std::unordered_map<llvm::Function *, int> functionEarlyExitMap;
+  std::unordered_map<llvm::Function *, std::optional<int>> functionEarlyExitMap;
 
   void initFunctionEarlyExitMap(char *filePath, llvm::Module &M);
-  bool replaceRetInst(llvm::Function &F, llvm::LLVMContext &CTX, int retval);
+  bool replaceRetInst(llvm::Function &F, llvm::LLVMContext &CTX,
+                      std::optional<int> retval);
 };
 
 #endif // LLVM2SMT_FORCE_FUNCTIONS_EARLY_EXIT_H
